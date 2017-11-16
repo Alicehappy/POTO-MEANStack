9b01e7418e0b9ae19f9faca438db3c1347cde68a
Alice <alicezou05@gmail.com>
2017-11-16 10:00:11 -0500

    Add Cookies Management: print all the cookies sent by the client.

--- server.js
+++ server.js
@@ -1,29 +1,10 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+var express      = require('express')
+var cookieParser = require('cookie-parser')
 
-// Create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var app = express()
+app.use(cookieParser())
 
-app.use(express.static('public'));
-app.get('/index.htm', function (req, res) {
-   res.sendFile( __dirname + "/" + "index.htm" );
+app.get('/', function(req, res) {
+   console.log("Cookies: ", req.cookies)
 })
-
-app.post('/process_post', urlencodedParser, function (req, res) {
-   // Prepare output in JSON format
-   response = {
-      first_name:req.body.first_name,
-      last_name:req.body.last_name
-   };
-   console.log(response);
-   res.end(JSON.stringify(response));
-})
-
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-
-   console.log("Example app listening at http://%s:%s", host, port)
-
-})
+app.listen(8081)
