8d5501b4ac69199e5a45ba118a68dacad0621df1
Alice <alicezou05@gmail.com>
2017-11-16 13:50:00 -0500

    Middleware: add a middleware function using both methods, and with/without a route.

--- server.js
+++ server.js
@@ -11,6 +11,29 @@ app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
+
+
+
+// An example middleware function
+var a_middleware_function = function(req, res, next) {
+  // ... perform some operations
+  next(); // Call next() so Express will call the next middleware function in the chain.
+};
+
+// Function added with use() for all routes and verbs
+app.use(a_middleware_function);
+
+// Function added with use() for a specific route
+app.use('/someroute', a_middleware_function);
+
+// A middleware function added for a specific HTTP verb and route
+app.get('/', a_middleware_function);
+
+
+
+
+
+
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!');
 });
