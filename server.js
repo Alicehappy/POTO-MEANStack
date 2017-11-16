e6ff0541383d603b0cadb88a654ca94191a9bf3a
Alice <alicezou05@gmail.com>
2017-11-16 11:19:05 -0500

    Express: Helloworld example.

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
-// Load HTTP module
-var http = require("http");
+var express = require('express');
+var app = express();
 
-// Create HTTP server and listen on port 8000 for requests
-http.createServer(function(request, response) {
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
 
-   // Set the response HTTP header with HTTP status and Content type
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-
-   // Send the response body "Hello World"
-   response.end('Hello World\n');
-}).listen(8000);
-
-// Print URL for accessing server
-console.log('Server running at http://127.0.0.1:8000/');
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!');
+});
